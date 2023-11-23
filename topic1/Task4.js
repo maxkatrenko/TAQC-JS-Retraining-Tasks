47b7957b09abc0915acb743197da69cc1db3700b
Max <mkatr@softserveinc.com>
2023-11-23 18:19:41 +0200

    additional checks for task4

--- topic1/Task4.js
+++ topic1/Task4.js
@@ -1,7 +1,7 @@
 
 const triange = (a, b, c) => {
 
-  if (a <= 0 || b <= 0 || c <= 0 || isNaN(a, b, c)) {
+  if (a <= 0 || b <= 0 || c <= 0 || isNaN(a, b, c) || a === null || b === null || c === null) {
     throw new Error('Incorrect data');
   }
 
