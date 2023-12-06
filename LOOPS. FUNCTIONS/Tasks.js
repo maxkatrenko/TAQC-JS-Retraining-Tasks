ad29a77ce92360985b64421eac3a7424c6add4b6
Max <mkatr@softserveinc.com>
2023-12-06 16:48:29 +0200

    loops.functions v2

--- LOOPS. FUNCTIONS/Tasks.js
+++ LOOPS. FUNCTIONS/Tasks.js
@@ -11,24 +11,36 @@ function createArray(start, end) {
 // console.log(createArray(1, 23));
 
 //2 (a<b)
-getNumbers = (a, b) => {
-  if (a < b) {
-    let ar = createArray(a, b);
-    for (let i = 0; ar.length; i++) {
-      for (let j = 0; j < ar[i]; j++) {
-        console.log(ar[i]);
-      }
-    }
-  } else {
-    throw new Error("'a' param must be less then 'b' param");
-  }
-}
-
-// getNumbers(1, 5);
+// getNumbers = (a, b) => {
+//   if (a < b) {
+//     let ar = createArray(a, b);
+//     for (let i = 0; ar.length; i++) {
+//       for (let j = 0; j < ar[i]; j++) {
+//         console.log(ar[i]);
+//       }
+//     }
+//   } else {
+//     throw new Error("'a' param must be less then 'b' param");
+//   }
+// }
 
 //3
 compact = (array) => {
-  return Array.from(new Set(array));
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] === array[j]) {
+        array.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return array;
 }
 
-// console.log(compact([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+//or simply
+// compact = (array) => {
+//   Array.from(new Set(array));
+//  return array;
+// }
+
+console.log(compact([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
